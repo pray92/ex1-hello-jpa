17ad316dadc4d98981b59ec434bebface44ea013
jisu Choi <pray92@users.noreply.github.com>
2021-10-10 14:58:35 +0900

    Hello JPA 실습, Maven 추가 설정

---
+++ src/main/java/hellojpa/Member.java
@@ -0,0 +1,30 @@
+package hellojpa;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity // 엔티티 선언, JPA 처음 로딩 시 JPA를 사용하는 클래스임을 인식
+//@Table(name ="USER")    // 특정 DB 테이블에 등록
+public class Member {
+
+    @Id // PK임을 알림
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
